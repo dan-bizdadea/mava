769e962c2fdd315c781f75e8bd418616760f2e11
Dan Bizdadea <dan.bizdadea@gmail.com>
2018-12-31 04:30:37 +0200

    new build

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -15,4 +15,14 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testAboutWithGoodUser()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about/jack');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('jack@mava.info', $crawler->filter('body')->text());
+    }
 }
